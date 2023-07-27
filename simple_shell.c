ce9281fa18344f581940202cad8b08972a2321fc
Mubarak1010 <anoze941@gmail.com>
2023-07-26 19:52:37 -0700

    changing my email and adding a file to check for task five

---
+++ simple_shell.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Nothing.
+ */
+
+int main(void)
+{
+	pid_t pid;
+	char *token, *buf = NULL, *args[64];
+	size_t size, track;
+	extern char **environ;
+
+	while (1)
+	{
+		if (getline(&buf, &size, stdin) == -1)
+		{
+			break;
+		}
+		buf[strcspn(buf, "\n")] = '\0';
+		token = strtok(buf, " ");
+		track = 0;
+		while (token != NULL && track < 63)
+		{
+			args[track] = token;
+			track++;
+			token = strtok(NULL, " ");
+		}
+		args[track] = NULL;
+		if (track == 0)
+			continue;
+		if (strcmp(args[0], "exit") == 0)
+			break;
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				perror("./shell");
+				break;
+			}
+		}
+		else
+			wait(NULL);
+	}
+	free(buf);
+	return (0);
+}
